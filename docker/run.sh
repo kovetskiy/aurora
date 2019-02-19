17a85403cdc6b49f470cedd552b5474c2cff66ed
Alexey Baranov <baranov.alxy@gmail.com>
2019-02-19 16:46:54 +0300

    add Dockerfile

---
+++ docker/run.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -euo pipefail
+
+pkg=$AURORA_PACKAGE
+
+rm /var/lib/pacman/db.lck 2> /dev/null \
+        || true
+
+pacman -Sy --noconfirm
+
+sudo -u nobody mkdir /app/build/$pkg
+
+sudo -u nobody git clone https://aur.archlinux.org/$pkg.git /app/build/$pkg
+
+cd /app/build/$pkg && sudo -u nobody makepkg --syncdeps --noconfirm
+
+cp -r /app/build/$pkg/*.pkg.* /aurora
+
+exit 0
